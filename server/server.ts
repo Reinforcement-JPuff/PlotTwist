8cacac2d4289bc4905be4252ecd65280f6246e0c
jacob-jpg1 <jlalexander102@gmail.com>
2025-03-15 12:22:28 -0500

    fixing branches

--- server/server.ts
+++ server/server.ts
@@ -15,17 +15,17 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../assets')));
 
 // route for root
-app.get('/', (req: any, res: any) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // global error handler
-app.use((err: any, req: any, res: any, next: any) => {
-    const defaultError = {
-        log: 'Express error handler caught an error in middleware',
-        status: 500,
-        message: { err: 'An error occurred' }
-    }
+app.use((err, req, res, next) => {
+  const defaultError = {
+      log: 'Express error handler caught an error in middleware',
+      status: 500,
+      message: { err: 'An error occurred' }
+  }
 });
 
 app.listen(PORT, () => {
